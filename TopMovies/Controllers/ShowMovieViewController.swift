8435e9ca507ce1ab5804cfd970e218b0890f9132
Medi Assumani <mediassumani49@gmail.com>
2018-03-25 20:51:06 -0400

    fixing pod issue

--- TopMovies/Controllers/ShowMovieViewController.swift
+++ TopMovies/Controllers/ShowMovieViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ShowMovieViewController.swift
 //  TopMovies
 //
 //  Created by Assumani, Medi on 3/25/18.
@@ -9,6 +9,24 @@
 import UIKit
 
 class ShowMovieViewController: UIViewController {
+    
+    // - MARK : @IBOUTLETS
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var rightsOfOwnerLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var moviePosterView: UIImageView!
+    
+    
+    
+
+    // - MAKR : @IBACTIONS
+
+    @IBAction func ituneLinkButtonIsTaped(_ sender: Any) {
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
